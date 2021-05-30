22ed60ae4f09277d3f0ae9ac0a05d38f36606d59
Berkcan <74976052+KB-Silence@users.noreply.github.com>
2021-05-30 20:35:56 +0300

    Position Entity Update 2021-05-30
    Position tablosunda OneToMany ilişkisi için değişiklikler yapıldı.

--- hrms/src/main/java/kilobyte/hrms/entities/concretes/Position.java
+++ hrms/src/main/java/kilobyte/hrms/entities/concretes/Position.java
@@ -1,11 +1,16 @@
 package kilobyte.hrms.entities.concretes;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,5 +30,9 @@ public class Position {
 	
 	@Column(name="position_name")
 	private String positionName;
+	
+	@OneToMany(mappedBy="position")
+	@JsonIgnore
+	private List<JobAdvertisement> advertisements;
 	
 }
