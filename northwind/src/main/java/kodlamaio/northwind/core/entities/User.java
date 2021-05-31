902f93fb92cf2a0ec90ae62564c848ea1a02a067
Berkcan <74976052+KB-Silence@users.noreply.github.com>
2021-05-31 13:54:12 +0300

    Northwind Projesi 8. Gün Kodları.

---
+++ northwind/src/main/java/kodlamaio/northwind/core/entities/User.java
@@ -0,0 +1,40 @@
+package kodlamaio.northwind.core.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import com.sun.istack.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "email")
+	@Email
+	@NotNull
+	@NotBlank
+	private String email;
+	
+	@NotNull
+	@NotBlank
+	@Column(name = "password")
+	private String password;
+}
