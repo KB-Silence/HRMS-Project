d9bb2674dcdf5b1bd0ca9f09e09dd07520109eba
KB-Silence <berkcanserbest5@gmail.com>
2021-06-08 06:23:46 +0300

    Mini Fix
    Bütün controller sınıflara @CrossOrigin anotasyonu eklendi.
    --
    CvDto sınıfında kullanılmayan import silindi.

--- hrms/src/main/java/kilobyte/hrms/api/controllers/UnemployedsController.java
+++ hrms/src/main/java/kilobyte/hrms/api/controllers/UnemployedsController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +17,7 @@ import kilobyte.hrms.entities.concretes.Unemployed;
 
 @RestController
 @RequestMapping("/api/unemployeds")
-
+@CrossOrigin
 public class UnemployedsController {
 
 	private UnemployedService unemployedService;
