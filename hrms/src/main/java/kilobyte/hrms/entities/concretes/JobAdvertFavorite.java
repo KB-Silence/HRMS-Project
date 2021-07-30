923bf2be815c435769f4742ae2ec62bc1657d221
KB-Silence <berkcanserbest5@gmail.com>
2021-07-31 01:50:23 +0300

    Concretes
    - Validation düzenlemeleri.
    - NotBlank anotasyonu kaynaklı hata için düzenlemeler.
    - User sınıfında "id" kullanımından kaynaklı hatadan dolayı UserId olarak değiştirildi.

--- hrms/src/main/java/kilobyte/hrms/entities/concretes/JobAdvertFavorite.java
+++ hrms/src/main/java/kilobyte/hrms/entities/concretes/JobAdvertFavorite.java
@@ -18,7 +18,6 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class JobAdvertFavorite {
 	
 	@Id
--- hrms/src/main/java/kilobyte/hrms/entities/concretes/Unemployed.java
+++ hrms/src/main/java/kilobyte/hrms/entities/concretes/Unemployed.java
@@ -19,7 +19,6 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-
 @Entity
 @Table(name = "unemployeds")
 @PrimaryKeyJoinColumn(name = "user_id")
@@ -27,49 +26,52 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = false)
-public class Unemployed extends User{
-	
+public class Unemployed extends User {
+
 	@NotBlank
 	@NotNull
 	@Column(name = "first_name")
 	private String firstName;
-	
+
 	@NotBlank
 	@NotNull
 	@Column(name = "last_name")
 	private String lastName;
-	
+
 	@NotBlank
 	@NotNull
 	@JsonIgnore
 	@Column(name = "nationality_id")
 	private String nationalityId;
-	
-	@NotBlank
+
 	@NotNull
 	@Column(name = "birth_date")
 	private LocalDate birthDate;
-	
+
 	@OneToMany(mappedBy = "unemployed")
 	@JsonIgnore
 	private List<Language> languages;
-	
+
 	@OneToMany(mappedBy = "unemployed")
 	@JsonIgnore
 	private List<Link> links;
-	
+
 	@OneToMany(mappedBy = "unemployed")
 	@JsonIgnore
 	private List<JobExperience> jobExperiences;
-	
+
 	@OneToMany(mappedBy = "unemployed")
 	@JsonIgnore
 	private List<CoverLetter> coverLetters;
-	
+
 	@OneToMany(mappedBy = "unemployed")
 	@JsonIgnore
 	private List<Technology> technologies;
-	
+
+	@OneToMany(mappedBy = "unemployed")
+	@JsonIgnore
+	private List<JobAdvertFavorite> advertFavorites;
+
 	@OneToOne(mappedBy = "unemployed")
 	@JsonIgnore
 	private Photo photos;
